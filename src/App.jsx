58470d200d05acc3f8180aa1baff2177877e6b88
Kuldeepkant26 <picasuplayers@gmail.com>
2025-08-13 01:41:53 +0530

    Added new cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Home from './pages/Home'
+import Home2 from './pages/Home2'
 import About from './pages/About'
-import ProductsPage from './pages/ProductsPage'
-import ProductDetailPage from './pages/ProductDetailPage'
-import CartSidebar from './components/CartSidebar'
 import Footer from './components/Footer'
+import Ribbons from '../Reactbits/Ribbons/Ribbons'
 
 function App() {
   return (
     <>
+      <Ribbons
+        baseThickness={24}
+        colors={["#01BFBD", "#00A8A6", "#008A88"]}
+        speedMultiplier={0.5}
+        maxAge={500}
+        enableFade={true}
+        enableShaderEffect={true}
+      />
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home2 />} />
         <Route path='/about' element={<About />} />
-        <Route path='/products' element={<ProductsPage />} />
-        <Route path='/product/:id' element={<ProductDetailPage />} />
       </Routes>
-      <CartSidebar />
       <Footer />
     </>
   )
---
+++ src/pages/Home2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../components/Hero'
+
+import Ribbons from '../../Reactbits/Ribbons/Ribbons'
+function Home2() {
+    return (
+        <>
+    
+          
+           
+            <Hero />
+
+        </>
+    )
+}
+
+export default Home2
